8922d81eacd96ed507d98dfe9da8bac46ec4c9b7
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-01-16 12:26:27 +0100

    script to query reddit api for number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+a function that queries the reddit api and returns
+the numbers of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that returns number of subscribers
+    in an reddict api
+    Args:
+        subreddit (string): the subreddit
+    Returns:
+        return number of subscribers"""
+
+    # reddit api endpoint url for subreddit
+    endpoint = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # set a custom user-agent to avoid too many request error
+    headers = {
+        'User-Agent': 'simple_app (by /u/TechieDermMD)'
+        }
+
+    response = requests.get(endpoint, headers=headers)
+
+    if response.status_code == 200:
+        subreddit_data = response.json()['data']
+        subscribers_count = subreddit_data['subscribers']
+        return subscribers_count
+    else:
+        return 0
