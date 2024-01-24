d6bdba7b0512f46ca71fc18c732992b19d0beeba
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-01-24 13:34:59 +0000

    increase limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Increase traffic amount that NGINX can handle by increasing the ULIMIT
+
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+#->
+# Restart Nginx
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
+
